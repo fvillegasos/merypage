1c149b1cd43608307fe632d3dedee4ad44283332
fvillegasos <fvillegasos.developer@gmail.com>
2025-06-22 16:30:06 +0200

    feat: add images + animations for yes-btn

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 const noBtn = document.getElementById('no-btn');
+const yesBtn = document.getElementById('yes-btn');
+const modal = document.getElementById('image-modal');
+const modalImg = document.getElementById('modal-img');
+const closeBtn = document.querySelector('.close');
+
 noBtn.style.position = 'absolute';
 
 noBtn.addEventListener('mouseenter', () => {
@@ -12,3 +17,20 @@ noBtn.addEventListener('mouseenter', () => {
     noBtn.style.left = `${randomLeft}px`;
     noBtn.style.top = `${randomTop}px`;
 });
+
+yesBtn.addEventListener('click', () => {
+    modal.style.display = "block";
+    modalImg.src = 'images/kittens_kissing.jpg';
+});
+
+// Close the modal when the close button is clicked
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Optional: Close the modal when clicking outside the image
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
